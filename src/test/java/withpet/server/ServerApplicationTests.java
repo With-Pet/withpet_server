59978f50690baf84d62da8b34066732a4e94b5e3
HyungJooKim <hyungjookim12@gmail.com>
2022-07-30 22:18:22 +0900

    [:wrench: config] 프로젝트 초기 구성
    1. gradle 설정
    2. package 구성
    3. application.yml 파일 생성
    4. docker-compose.yml 파일 생성

---
+++ src/test/java/withpet/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package withpet.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
